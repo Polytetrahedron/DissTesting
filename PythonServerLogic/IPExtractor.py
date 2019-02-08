e11fe04fdfc6f37617a060fe1b268a8409ba5f3a
Mark <mrk447@gmail.com>
2019-02-08 14:21:54 +0000

    IP Extractor working and finished

---
+++ PythonServerLogic/IPExtractor.py
@@ -0,0 +1,32 @@
+import socket
+import time
+
+def extract_local_IP(timeout = 0):
+    """
+    This method retrieves a list of network interfaces and polls them to find the current in use local
+    address. The IP is assumed to be a standard IPv4 "class" A, B, or C address
+    """
+    interfaces = socket.gethostbyname_ex(socket.gethostname())
+    user_address = "null"
+
+    timeout_counter = timeout
+    max_retry = 5
+
+    for current in interfaces:
+        for i in current:
+            if i.isalpha() == False:
+                if i.startswith("127.") == False and i.startswith("169.") == False: # internal and loopback
+                    if i.startswith("192.") == True or i.startswith("172.") == True or i.startswith("10.") == True: # class A. B, and C addresses
+                        print("Acquired DHCP address: " + str(i)) # print IP address to console
+                        return i # report IP of device
+                    else:
+                        if timeout_counter == 0:
+                            print("ERROR: Could not acquire DHCP address")
+                            if(timeout_counter != max_retry):
+                                timeout_counter += 1
+                                print("Retrying IP Acquisition " + str(timeout_counter) + "/" + str(max_retry))
+                                time.sleep(2)
+                                extract_local_IP(timeout_counter)
+                            else:
+                                user_address = input("Failed to set server IP via DHCP, please provide one: ")
+                                return user_address
---
+++ PythonServerLogic/ServerHandler.py
@@ -0,0 +1,17 @@
+import grpc
+import concurrent
+import socket
+import subprocess
+
+#These are the custom behavior modules
+import ClientScanner, EmailModule, NewsModule, TimeModule, WeatherModule, IPExtractor
+
+std_server_port = "5156"
+std_listening_port = "7890"
+local_dhcp_address = IPExtractor.extract_local_IP()
+
+def serve():
+    server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=10))
+    #grpc interface 
+    server.add_insecure_port(local_dhcp_address + ':' + std_server_port)
+    server.start()
